09091d1c8cddaac587bcd675134d48c75013fe68
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-24 10:59:18 -0400

    This commit includes an updated movie list as well as utilities for user input

---
+++ DevBuild.MovieList/Movie.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuild.MovieList {
+
+    public enum MovieCategory { Animated, Action, Comedy, Drama, MartialArts, Horror, SciFi, Thriller }
+
+    class Movie {
+        public static List<string> movieCategories = new List<string>() { "Animated", "Action", "Comedy", "Drama", "Martial Arts", "Horror", "Sci-Fi" };
+
+        public string           Title { get; set; }
+        public string           Category { get; set; }
+        public short            Year { get; set; }                  // %@#$*&! remakes... >.<
+        public MovieCategory    MovieCategory { get; set; }
+
+        public Movie() {
+        }
+
+        public Movie(string movieTitle, string movieCategory) {
+            Title = movieTitle;
+            Category = movieCategory;
+        }
+
+        public override string ToString() {
+            //Let's use this method to fix the category enum, at least while printing 
+            return $"{ Title } : { Category }";
+        }
+
+        public static void DisplayMovieCategories([Optional]bool menuMode) {
+            for (int i = 0; i < movieCategories.Count; i++) {
+                Console.WriteLine((menuMode ? $"{i + 1}.) " : "") + movieCategories[i]);
+            }
+        }
+
+        public static void GetMoviesInCategory(IList<object> masterList, string category) {
+
+            ArrayList searchResults = new ArrayList();
+
+            if (movieCategories.Contains(category)) {
+                foreach (object c in masterList) {
+                    if ((c is Movie) && (c as Movie).Category == category) {
+                        searchResults.Add((c as Movie).Title);
+                        //Console.WriteLine(c);
+                    }
+                }
+            }
+            searchResults.Sort();
+
+            Console.WriteLine("\nSearch Results".PadLeft(8));
+            Console.WriteLine("******************************");
+            foreach (object c in searchResults) {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("******************************\n");
+        }
+    }
+}
--- DevBuild.MovieList/Program.cs
+++ DevBuild.MovieList/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevBuild.Utilities;
 
 namespace DevBuild.MovieList {
     class Program {
+
+        public static ArrayList ourMovies = new ArrayList();
+
         static void Main(string[] args) {
+            ourMovies.Add(new Movie("Titan AE", "Animated"));
+            ourMovies.Add(new Movie("An American Tail", "Animated"));
+            ourMovies.Add(new Movie("Robin Hood: Men in Tights", "Comedy"));
+            ourMovies.Add(new Movie("Get Out", "Horror"));
+            ourMovies.Add(new Movie("Fist of Legend", "Martial Arts"));
+            ourMovies.Add(new Movie("Tucker and Dale vs. Evil", "Horror"));
+            ourMovies.Add(new Movie("A Bug's Life", "Animated"));
+            ourMovies.Add(new Movie("Interstellar", "Sci-Fi"));
+            ourMovies.Add(new Movie("Contact", "Sci-Fi"));
+            ourMovies.Add(new Movie("The Imitation Game", "Drama"));
+            while (true) {
+                MenuHandling.PrintMenuOptions(Movie.movieCategories.ToArray(), menuMode: true);
+                uint userSelection = MenuHandling.SelectMenuOption(Movie.movieCategories.Count);
+                Movie.GetMoviesInCategory(ourMovies.ToArray(), Movie.movieCategories[(int)userSelection -1]);
+
+                YesNoAnswer yesNoAnswer = UserInput.GetYesOrNoAnswer("Do you want to search again? (y/n or yes/no) ");
+                switch (yesNoAnswer) {
+                    case YesNoAnswer.Yes: continue;
+                    case YesNoAnswer.No: return;
+                    default: continue;
+                }
+
+            }
         }
     }
 }
